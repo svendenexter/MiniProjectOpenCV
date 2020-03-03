d3c0e7ba6b1ed61d21e41a8eaa8413064f0852f3
stefanammerlaan <39700192+stefanammerlaan@users.noreply.github.com>
2020-03-03 14:10:42 +0100

    Add files via upload
    car detectection *

---
+++ autodetectie.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  3 13:50:44 2020
+
+@author: stefa
+"""
+
+import cv2
+import numpy as np
+import pafy
+import threading
+import time
+
+
+global timeinterval
+timeinterval = 1
+
+url = "https://www.youtube.com/watch?v=nVutZktUxfY"
+
+video=pafy.new(url)
+best=video.getbest(preftype="mp4")
+
+capture = cv2.VideoCapture()
+capture.open(best.url)
+starttime=time.time()
+
+
+#video from webcam
+#cap=cv2.VideoCapture(0)
+cap = cv2.VideoCapture('video.mp4')
+#car classifier
+car_cascade=cv2.CascadeClassifier('cars.xml')
+
+
+while (True) :
+    # zorgt voor juiste framerate 25 FPS (als code het bijhoud)
+    time.sleep((1/25) - ((time.time() - starttime) % (1/25))) 
+    capture = cv2.VideoCapture()
+    capture.open(best.url)
+    ret, frame = capture.read()
+    
+    if (frame is not None):
+        r=800/frame.shape[1]
+        dim = (800,int (frame.shape[0]*r))
+        resized = cv2.resize(frame,dim, interpolation = cv2.INTER_AREA)    
+        cv2.imshow('output',resized)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cars=car_cascade.detectMultiScale(gray,2,1)
+    
+    for(x,y,w,h) in cars:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+        font=cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame,'car',(x+6,y-6),font,0.5,(0,0,255),1)
+        
+        cv2.imshow('Car Detection',frame)
+    else:
+        
+        print ("geen frame")
+        
+    if (cv2.waitKey(1)== ord ('q')):
+        break
+    
+cap.release()
+cv2.destroyAllWindows
