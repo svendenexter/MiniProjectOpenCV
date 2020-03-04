cd8ff3f77751d2b29c8d3678a303771c51378ac0
stefanammerlaan <39700192+stefanammerlaan@users.noreply.github.com>
2020-03-04 13:25:13 +0100

    Add files via upload

--- Code_With_BackgroundSubtractor.py
+++ Code_With_BackgroundSubtractor.py
@@ -1,28 +1,41 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar  4 12:57:56 2020
+Created on Wed Mar  4 12:08:53 2020
 
-@author: jerqq
+@author: stefa
 """
+
+#import cv2
+#
+#
+#cap=cv2.VideoCapture(0)
+#while (True):
+#    
+#    ret, frame = cap.read()
+#    cv2.imshow('Car Detection',frame)
+#    if (cv2.waitKey(1)== ord ('q')):
+#        break
+#cap.release()
+#cv2.destroyAllWindows()
+
 
 import numpy as np
 import cv2
+cap = cv2.VideoCapture('road.mp4')
+#cap = cv2.VideoCapture('road.mp4')
 
-cap = cv2.VideoCapture('cars.mp4')
-frame = cap[300:, 700:1300]
 
-#fgbg = cv2.createBackgroundSubtractorMOG()
+
 fgbg = cv2.createBackgroundSubtractorMOG2()
-
 while(1):
     ret, frame = cap.read()
-
-    fgmask = fgbg.apply(frame)
-
-    cv2.imshow('frame',fgmask)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
+    frame = frame[300:, 700:1300]
+    if (ret == True):        
+        fgmask = fgbg.apply(frame)
+        cv2.imshow('frame',fgmask)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
 
 cap.release()
 cv2.destroyAllWindows()
