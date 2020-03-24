66372b6ad714585735a50b8125c89df9cfb38843
jerekorpela <61691340+jerekorpela@users.noreply.github.com>
2020-03-24 14:27:26 +0100

    Update BEERCAPS.py

--- BEERCAPS.py
+++ BEERCAPS.py
@@ -52,21 +52,34 @@ def finding_circles(image_search,image_draw):
 
 
 
-## EXTRACTING CAPS FROM PICTURE
-
-    
-    
+## EXTRACTING CAPS FROM PICTURE   
 def extract_circles(image,picture_caps):
     
+    # Circle detection
     circles = cv2.HoughCircles(image,cv2.HOUGH_GRADIENT,1.5,140,
-                            param1=20,param2=10,minRadius=45,maxRadius=55)
-
+                        param1=20,param2=10,minRadius=45,maxRadius=55)
+    
+    # Converts circles to unit16
     circles = np.uint16(np.around(circles))
     
-    print(circles)
+    # Creates array for extracted caps
+    cropped=[0]
     
-    cropped=picture_caps[261:360,231:330]
-    
+    # Moves circles location  to variables and extract caps from picture
+    for i in ((circles[0])) :
+        
+        X=i[0]
+        Y=i[1]
+        R=i[2]
+      
+       
+        rectX = (X - R) 
+        rectY = (Y - R)
+        cropped.append(picture_caps[rectY:(rectY+2*R), rectX:(rectX+2*R)])
+        
+    # Removes arrays firsts index (because its 0)
+    cropped=cropped[1:]
+           
     return cropped
 
 
@@ -84,7 +97,7 @@ def extract_circles(image,picture_caps):
 converted_image=convert_image(resize,blur=3)
 
 # finding circles
-founded_caps=finding_circles(converted_image,resize)
+founded_caps =finding_circles(converted_image,resize)
 
 # extract caps
 extracted_caps=extract_circles(converted_image,resize)
@@ -97,14 +110,15 @@ extracted_caps=extract_circles(converted_image,resize)
 
 cv2.imshow('Converted picture',converted_image)
 cv2.imshow('Founded caps',founded_caps)
-cv2.imshow('Cropped caps',extracted_caps)
+
+   
+cv2.imshow("1",extracted_caps[0])
+cv2.imshow("2",extracted_caps[1])
+cv2.imshow("3",extracted_caps[2])
+cv2.imshow("4",extracted_caps[3])
+cv2.imshow("5",extracted_caps[4])
+cv2.imshow("6",extracted_caps[5])
 
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
-
-
-
